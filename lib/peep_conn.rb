841eb332e2df3ef0a0ee667c884194c49bccb7a6
SDRACK <steve@finerylondon.com>
2017-05-02 16:27:39 +0100

    Attempt to overcome un-included files

--- lib/peep_conn.rb
+++ lib/peep_conn.rb
@@ -1,5 +1,5 @@
-# Dir[File.join(File.dirname(__FILE__), 'lib/peep_conn/*.rb')]
-#   .each { |f| require f.split('/').last(2).join('/') }
+Dir[File.join(File.dirname(__FILE__), 'lib/peep_conn/*.rb')]
+  .each { |f| require f.split('/').last(2).join('/') }
 # require 'savon'
 # require 'active_support/core_ext/object'
 
