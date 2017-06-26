408eb50031d84efa0318c08a8f20a173b4ddf7e3
SDRACK <steve@finerylondon.com>
2017-06-26 17:30:12 +0100

    Add more and more tables from PV

--- lib/peep_conn/constants.rb
+++ lib/peep_conn/constants.rb
@@ -4,19 +4,33 @@ module PeepConn
   # Link our instance with applicable PeopleVox table
   # Update: additional tables added not linked to instances, but following
   # the same singular resource name
-  TABLE_NAMES = { 'address' => 'Customer addresses',
+  TABLE_NAMES = { 'carrier' => 'Carriers', #
+                  'address' => 'Customer addresses',
                   'user' => 'Customers',
+                  'dispatch_package' => 'Despatch packages', #
                   'dispatch' => 'Despatches',
+                  'item_type_group' => 'Item type groups', #
+                  'item_type_kitting' => 'Item type kittings', #
                   'line_item_supplier' => 'Item type suppliers',
+                  'item_type_unit' => 'Item type units', #
+                  'location_group_type' => 'Location group types', #
                   'product' => 'Item types',
                   'location_group' => 'Location groups',
                   'location' => 'Locations',
+                  'package_type' => 'Package types', #
+                  'payment_method' => 'Payment methods', #
                   'purchase_order_item' => 'Purchase order items',
                   'purchase_order' => 'Purchase orders',
                   'removal' => 'Removals',
                   'return_reason' => 'Return reasons',
+                  # 'return' => 'Returns', # Currently broken in PV, can't configure
+                  'role' => 'Roles', #
                   'line_item' => 'Sales order items',
                   'order' => 'Sales orders',
+                  'service_type' => 'Service types', #
+                  'site' => 'Sites', #
+                  'supplier_address' => 'Supplier addresses', #
                   'supplier' => 'Suppliers',
+                  'tax_code' => 'Tax codes', #
                   'unit_type' => 'Unit types' }.freeze
 end
