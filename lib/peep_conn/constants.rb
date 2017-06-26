c46ae1c1a3054fb942804674c9466194b20ff529
SDRACK <steve@finerylondon.com>
2017-06-26 16:25:32 +0100

    Add additional Peoplevox tables to TABLE_NAMES constant

--- lib/peep_conn/constants.rb
+++ lib/peep_conn/constants.rb
@@ -2,11 +2,21 @@ module PeepConn
   VERSION = '0.0.1'.freeze
 
   # Link our instance with applicable PeopleVox table
-  TABLE_NAMES = {
-    'user' => 'Customers',
-    'address' => 'Customer addresses',
-    'order' => 'Sales orders',
-    'line_item' => 'Sales order items',
-    'products' => 'Item types'
-  }.freeze
+  # Update: additional tables added not linked to instances, but following
+  # the same singular resource name
+  TABLE_NAMES = { 'address' => 'Customer addresses',
+                  'user' => 'Customers',
+                  'dispatch' => 'Despatches',
+                  'line_item_supplier' => 'Item type suppliers',
+                  'product' => 'Item types',
+                  'location_group' => 'Location groups',
+                  'location' => 'Locations',
+                  'purchase_order_item' => 'Purchase order items',
+                  'purchase_order' => 'Purchase orders',
+                  'removal' => 'Removals',
+                  'return_reason' => 'Return reasons',
+                  'line_item' => 'Sales order items',
+                  'order' => 'Sales orders',
+                  'supplier' => 'Suppliers',
+                  'unit_type' => 'Unit types' }.freeze
 end
