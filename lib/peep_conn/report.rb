bdacaa368b9ec733f1a0d9481c402bff57e113fd
SDRACK <steve@finerylondon.com>
2017-07-27 12:50:15 +0100

    Add outstanding orders report

--- lib/peep_conn/report.rb
+++ lib/peep_conn/report.rb
@@ -13,5 +13,19 @@ module PeepConn
         } }
       ).body
     end
+
+    def outstanding_wholesale_orders(opts = {})
+      client.call(
+        :get_report_data,
+        message: { getReportRequest: {
+          TemplateName: 'Outstanding sales orders',
+          PageNo: opts[:page] || 1,
+          ItemsPerPage: opts[:per_page] || 0,
+          # Currently pulling all data, as columns / filters aren't working
+          # Columns: '[Item code], [Available]',
+          # FilterClause: opts[:term] || 'Site reference Equals "Wholesale"'
+        } }
+      ).body
+    end
   end
 end
