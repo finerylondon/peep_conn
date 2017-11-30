3e5ecc905ea365de398102538c6c1653f26c902e
Neil Turner <neil@neilturner.me>
2017-11-30 17:18:47 +0000

    Add additional report

--- lib/peep_conn/report.rb
+++ lib/peep_conn/report.rb
@@ -26,6 +26,20 @@ module PeepConn
       ).body
     end
 
+    def item_inventory_by_location(opts = {})
+      client.call(
+        :get_report_data,
+        message: { getReportRequest: {
+          TemplateName: 'Item inventory by location',
+          PageNo: opts[:page] || 1,
+          ItemsPerPage: opts[:per_page] || 0,
+          # Currently pulling all data, as columns / filters aren't working
+          # Columns: '[Item code], [Available]',
+          # FilterClause: opts[:term] || 'Site reference Equals "PrimarySite"'
+        } }
+      ).body
+    end
+
     def outstanding_wholesale_orders(opts = {})
       client.call(
         :get_report_data,
